dbf9541341a772a7701ae4ea06ccbc81a7298203
jang <hyoje89@naver.com>
2023-05-04 22:33:10 +0900

    #002 synchronized를 이용한 동시요청 처리

--- src/main/java/com/example/til/동시성이슈/Service/StockService.java
+++ src/main/java/com/example/til/동시성이슈/Service/StockService.java
@@ -14,8 +14,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
     // 재고감소 메소드를 만들기위해 Stock 아이디와 수량을 받아서 생성
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+    //@Transactional --> decrease 메서드가 끝났지만 트랜잭션이 종료되기전에 decrease 메서드를 실행하게 되면 정확한 데이터를 가져가지 못함
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
--- src/test/java/com/example/til/동시성이슈/Service/StockServiceTest.java
+++ src/test/java/com/example/til/동시성이슈/Service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class StockServiceTest {
@@ -40,5 +44,30 @@ class StockServiceTest {
         //then
         // 100 - 1 = 99
         assertEquals(99, stock.getQuantity());
+    }
+
+    // 요청이 동시에 여러개가 들어왔을때
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 다른 쓰레드에서 수행중인 작업이 완려될때 까지 대기할수 있도록
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                }finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L, stock.getQuantity());
     }
 }
